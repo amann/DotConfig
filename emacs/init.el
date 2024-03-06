6af53b0299b3563d3a2ccf2aff218f1091f0a8c2
Olivier <olivier.amann@swisslife.ch>
2024-03-06 15:18:50 +0100

    emacs/init.org: simplify configuration of dired.

--- emacs/init.el
+++ emacs/init.el
@@ -500,12 +500,7 @@ parses its input."
   :commands (dired dired-jump)
   :bind (("C-x C-j" . dired-jump))
   :custom
-  (dired-listing-switches "-AGFhlv --group-directories-first --time-style=iso --dired --color")
-  (dired-mouse-drag-files t)
-  :config
-  '(evil-collection-define-key 'normal 'dired-mode-map
-    "h" 'dired-single-up-directory
-    "l" 'dired-single-buffer))
+  (dired-listing-switches "-AGFhlv --group-directories-first --time-style=iso --dired --color"))
 
 (use-package dired-single)
 
@@ -522,17 +517,9 @@ parses its input."
 (use-package all-the-icons-dired
   :hook (dired-mode . all-the-icons-dired-mode))
 
-(use-package dired-open
-  :config
-  ;; Doesn't work as expected!
-  ;;(add-to-list 'dired-open-functions #'dired-open-xdg t)
-  (setq dired-open-extensions '(("png" . "feh")
-                                ("mkv" . "mpv"))))
+(use-package dired-open)
 
 (use-package dired-hide-dotfiles
-  :hook (dired-mode . dired-hide-dotfiles-mode)
-  :config
-  '(evil-collection-define-key 'normal 'dired-mode-map
-    "H" 'dired-hide-dotfiles-mode))
+  :hook (dired-mode . dired-hide-dotfiles-mode))
 
 (load custom-file t t t t))
